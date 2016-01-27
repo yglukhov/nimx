c2e91a47ba71cc982e4c5abf0f75b31934fbddaa
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2016-01-27 14:19:21 +0200

    Added pathutils module

---
+++ nimx/pathutils.nim
@@ -0,0 +1,65 @@
+import strutils
+
+proc relativePathToPath*(path, toPath: string): string =
+    # Returns a relative path to `toPath` which is equivalent of absolute `path`
+    # E.g. given `path` = "/a/b/c/d/e" and `toPath` = "/a/b/c/f/g"
+    # result = ""
+
+    let pc = path.split('/')
+    let tpc = toPath.split('/')
+
+    let ln = min(pc.len, tpc.len)
+    var cp = 0
+    while cp < ln:
+        if pc[cp] != tpc[cp]: break
+        inc cp
+
+    var ccp = pc.len - cp
+    result = ""
+    while ccp > 0:
+        result &= "../"
+        dec ccp
+    while cp < tpc.len:
+        result &= tpc[cp]
+        if cp != tpc.len - 1:
+            result &= "/"
+        inc cp
+
+proc normalizePath*(path: var string) =
+    let ln = path.len
+    var j = 0
+    var i = 0
+
+    template isSep(c: char): bool = c == '/' or c == '\\'
+    template rollback() =
+        dec j
+        if j < 0:
+            raise newException(Exception, "Path is too relative: " & path)
+        while j > 0:
+            if path[j].isSep: break
+            dec j
+
+    while i < ln:
+        var copyChar = true
+        if path[i].isSep:
+            if ln > i + 1:
+                if path[i + 1] == '.':
+                    if ln > i + 2:
+                        if path[i + 2] == '.':
+                            rollback()
+                            copyChar = false
+                            i += 3
+                            if j == 0: inc i
+                        elif path[i + 2].isSep:
+                            copyChar = false
+                            i += 2
+        if copyChar:
+            path[j] = path[i]
+            when defined(windows):
+                if path[j] == '/': path[j] = '\\'
+            else:
+                if path[j] == '\\': path[j] = '/'
+
+            inc j
+            inc i
+    path.setLen(j)
--- nimx/resource.nim
+++ nimx/resource.nim
@@ -4,6 +4,7 @@ import streams
 import json
 import tables
 import async_http_request
+import pathutils
 
 when not defined(js):
     import os
@@ -23,45 +24,6 @@ proc pathForResource*(name: string): string
 
 template registerResource*[T](c: var ResourceCache[T], name: string, r: T) =
     c.cache[pathForResource(name)] = r
-
-proc normalizePath(path: var string) =
-    let ln = path.len
-    var j = 0
-    var i = 0
-
-    template isSep(c: char): bool = c == '/' or c == '\\'
-    template rollback() =
-        dec j
-        if j < 0:
-            raise newException(Exception, "Path is too relative: " & path)
-        while j > 0:
-            if path[j].isSep: break
-            dec j
-
-    while i < ln:
-        var copyChar = true
-        if path[i].isSep:
-            if ln > i + 1:
-                if path[i + 1] == '.':
-                    if ln > i + 2:
-                        if path[i + 2] == '.':
-                            rollback()
-                            copyChar = false
-                            i += 3
-                            if j == 0: inc i
-                        elif path[i + 2].isSep:
-                            copyChar = false
-                            i += 2
-        if copyChar:
-            path[j] = path[i]
-            when defined(windows):
-                if path[j] == '/': path[j] = '\\'
-            else:
-                if path[j] == '\\': path[j] = '/'
-
-            inc j
-            inc i
-    path.setLen(j)
 
 var warnWhenResourceNotCached* = false
 
