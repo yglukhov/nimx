5109202441c41c3f688e151f4a38a82282fd668d
Yuriy Glukhov <yutiy.glukhov@gmail.com>
2016-03-02 14:23:01 +0200

    Added pause and resume to timers

--- nimx/timer.nim
+++ nimx/timer.nim
@@ -1,79 +1,149 @@
+import times
 
 when defined(js):
-    type Timer* {.importc.} = ref object
+    type TimerID {.importc.} = ref object
+
+    proc setInterval(p: proc(), timeout: float): TimerID {.importc.}
+    proc setTimeout(p: proc(), timeout: float): TimerID {.importc.}
+    proc clearInterval(t: TimerID) {.importc.}
+    proc clearTimeout(t: TimerID) {.importc.}
+
 else:
-    import sdl2, sdl_perform_on_main_thread
-    type Timer* = ref object
-        timer: TimerID
-        callback: proc()
-        isPeriodic: bool
-        cancelled: bool
+    import sdl2, sdl_perform_on_main_thread, tables
+
+type Timer* = ref object
+    callback: proc()
+    timer: TimerID
+    nextFireTime: float
+    interval: float
+    isPeriodic: bool
+    isRescheduling: bool
+    when not defined(js):
+        id: int
+
+when not defined(js):
+    # Due to SDL timers are running on a separate thread there is no way to
+    # safely pass pointers/references as timer context because there is no
+    # way to tell which thread touches the context last and has to dispose it.
+    # Solution to this is a global map of active timers.
+    var allTimers = initTable[int, Timer]()
+
+    proc deleteTimerFromSDL(t: Timer) =
+        discard removeTimer(t.timer)
+        t.timer = 0
+        allTimers.del(t.id)
+        t.id = 0
+
+template isTimerValid(t: Timer): bool =
+    when defined(js):
+        not t.timer.isNil
+    else:
+        t.timer != 0
 
 proc clear*(t: Timer) =
     if not t.isNil:
-        when defined(js):
-            asm """
-            if (`t`.__nimx_periodic) {
-                clearInterval(`t`);
-            }
-            else {
-                clearTimeout(`t`);
-            }
-            """
-        else:
-            t.cancelled = true
+        if t.isTimerValid:
+            when defined(js):
+                if t.isPeriodic and not t.isRescheduling:
+                    clearInterval(t.timer)
+                else:
+                    clearTimeout(t.timer)
+                t.timer = nil
+            else:
+                t.deleteTimerFromSDL()
+            t.nextFireTime = t.nextFireTime - epochTime()
+
+template fireCallbackAux(t: Timer) =
+    t.callback()
+    if t.isPeriodic:
+        t.nextFireTime = epochTime() + t.interval
+        if t.isRescheduling:
+            discard
 
 when not defined(js):
-    proc finalizeCallback(data: pointer) {.cdecl.} =
-        let t = cast[Timer](data)
-        GC_unref(t)
+    proc nextTimerId(): int =
+        var idCounter {.global.} = 0
+        inc idCounter
+        result = idCounter
 
-    proc fireAndFinalizeCallback(data: pointer) {.cdecl.} =
-        let t = cast[Timer](data)
-        if not t.cancelled:
-            t.callback()
-        GC_unref(t)
+    proc timeoutThreadCallback(interval: uint32, data: pointer): uint32 {.cdecl.}
 
-    proc fireCallback(data: pointer) {.cdecl, gcsafe.} =
-        let t = cast[Timer](data)
-        if not t.cancelled:
-            t.callback()
+    proc fireCallback(data: pointer) {.cdecl.} =
+        let t = allTimers.getOrDefault(cast[int](data))
+        if not t.isNil:
+            t.fireCallbackAux()
+            if not t.isPeriodic:
+                t.deleteTimerFromSDL()
+            elif t.isRescheduling:
+                t.deleteTimerFromSDL()
+                t.id = nextTimerId()
+                t.isRescheduling = false
+                allTimers[t.id] = t
+                t.timer = addTimer(uint32(t.interval * 1000), timeoutThreadCallback, cast[pointer](t.id))
+
 
     # Nim is hostile when it's callbacks are called from an "unknown" thread.
     # The following function can not use nim's stack trace and GC.
     {.push stack_trace:off.}
-    proc timeoutThreadCallback(interval: uint32, data: pointer): uint32 {.cdecl, thread.} =
-        let t = cast[Timer](data)
-        if t.cancelled:
-            performOnMainThread(finalizeCallback, data)
-        elif t.isPeriodic:
-            performOnMainThread(fireCallback, data)
-            result = interval
-        else:
-            performOnMainThread(fireAndFinalizeCallback, data)
+    proc timeoutThreadCallback(interval: uint32, data: pointer): uint32 =
+        # This proc is run on a foreign thread!
+        performOnMainThread(fireCallback, data)
+        result = interval
     {.pop.}
 
 proc newTimer*(interval: float, repeat: bool, callback: proc()): Timer =
-    doAssert(not callback.isNil)
+    assert(not callback.isNil)
+    result.new()
+    result.callback = callback
+    result.isPeriodic = repeat
+    result.nextFireTime = epochTime() + interval
+    result.interval = interval
+
     when defined(js):
-        asm """
-        `result` = `repeat` ? setInterval(`callback`, `interval` * 1000) : setTimeout(`callback`, `interval` * 1000);
-        `result`.__nimx_periodic = `repeat`;
-        """
+        let t = result
+        let cb = proc() =
+            fireCallbackAux(t)
+        if t.isPeriodic:
+            result.timer = setInterval(cb, interval * 1000)
+        else:
+            result.timer = setTimeout(cb, interval * 1000)
     else:
         var sdlInitialized {.global.} = false
         if not sdlInitialized:
             sdl2.init(INIT_TIMER)
             sdlInitialized = true
 
-        result.new()
-        result.callback = callback
-        result.isPeriodic = repeat
-        result.timer = addTimer(uint32(interval * 1000), timeoutThreadCallback, cast[pointer](result))
-        GC_ref(result)
+        result.id = nextTimerId()
+        allTimers[result.id] = result
+        result.timer = addTimer(uint32(interval * 1000), timeoutThreadCallback, cast[pointer](result.id))
 
 proc setTimeout*(interval: float, callback: proc()): Timer {.discardable.} =
     newTimer(interval, false, callback)
 
 proc setInterval*(interval: float, callback: proc()): Timer {.discardable.} =
     newTimer(interval, true, callback)
+
+proc pause*(t: Timer) = t.clear()
+
+proc resume*(t: Timer) =
+    if not t.isTimerValid:
+        when defined(js):
+            if t.isPeriodic:
+                let cb = proc() =
+                    t.fireCallbackAux()
+                    let newcb = proc() =
+                        t.fireCallbackAux()
+                    t.timer = setInterval(newcb, t.interval * 1000)
+                    t.isRescheduling = false
+
+                t.timer = setTimeout(cb, t.nextFireTime * 1000)
+                t.isRescheduling = true
+            else:
+                t.timer = setTimeout(t.callback, t.nextFireTime * 1000)
+        else:
+            t.id = nextTimerId()
+            t.isRescheduling = true
+            allTimers[t.id] = t
+            t.timer = addTimer(uint32(t.nextFireTime * 1000), timeoutThreadCallback, cast[pointer](t.id))
+
+        t.nextFireTime = t.nextFireTime + epochTime()
---
+++ test/sample06_timers.nim
@@ -0,0 +1,69 @@
+import strutils
+
+import nimx.view
+import nimx.timer
+import nimx.text_field
+import nimx.button
+import sample_registry
+
+type TimersSampleView = ref object of View
+    timer: Timer
+    intervalTextField: TextField
+
+method init(t: TimersSampleView, r: Rect) =
+    procCall t.View.init(r)
+
+    discard t.newLabel(newPoint(20, 20), newSize(120, 20), "interval: ")
+    let intervalTextField = t.newTextField(newPoint(150, 20), newSize(120, 20), "5")
+
+    discard t.newLabel(newPoint(20, 50), newSize(120, 20), "periodic: ")
+
+    let periodicButton = newCheckbox(newRect(150, 50, 20, 20))
+    t.addSubview(periodicButton)
+
+    var firesLabel: TextField
+
+    let startButton = newButton(newRect(20, 80, 100, 20))
+    startButton.title = "Start"
+    startButton.onAction do():
+        t.timer.clear()
+        firesLabel.text = "fires: "
+        t.timer = newTimer(parseFloat(intervalTextField.text), periodicButton.boolValue, proc() =
+            firesLabel.text &= "O"
+            firesLabel.setNeedsDisplay()
+            )
+    t.addSubview(startButton)
+
+    let clearButton = newButton(newRect(20, 110, 100, 20))
+    clearButton.title = "Clear"
+    clearButton.onAction do():
+        t.timer.clear()
+    t.addSubview(clearButton)
+
+    let pauseButton = newButton(newRect(20, 140, 100, 20))
+    pauseButton.title = "Pause"
+    pauseButton.onAction do():
+        t.timer.pause()
+    t.addSubview(pauseButton)
+
+    let resumeButton = newButton(newRect(20, 170, 100, 20))
+    resumeButton.title = "Resume"
+    resumeButton.onAction do():
+        t.timer.resume()
+    t.addSubview(resumeButton)
+
+    let secondsLabel = t.newLabel(newPoint(20, 200), newSize(120, 20), "seconds: ")
+    var secs = 0
+    setInterval 1.0, proc() =
+        inc secs
+        if secs >= 10:
+            secs = 0
+        secondsLabel.text = "seconds: "
+        for i in 0 ..< secs:
+            secondsLabel.text &= "O"
+        secondsLabel.setNeedsDisplay()
+
+    firesLabel = t.newLabel(newPoint(20, 230), newSize(120, 20), "fires: ")
+
+
+registerSample "Timers", TimersSampleView.new(newRect(0, 0, 100, 100))
