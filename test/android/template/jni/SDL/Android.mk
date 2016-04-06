7d0e30b5b33da9fd89c299ad2c3df720c51d7f26
rrenderr <rrenderr@gmail.com>
2016-04-06 17:39:37 +0300

    - добавлен OGL enum DST_COLOR и ф-я блендинга blendFuncSeparate
    - при сборке теперь скитается папка build
    
    - SDLActivity обновлены до последней версии SDL

--- test/android/template/jni/SDL/Android.mk
+++ test/android/template/jni/SDL/Android.mk
@@ -21,12 +21,13 @@ LOCAL_SRC_FILES := \
 	$(wildcard $(LOCAL_PATH)/src/audio/android/*.c) \
 	$(wildcard $(LOCAL_PATH)/src/audio/dummy/*.c) \
 	$(LOCAL_PATH)/src/atomic/SDL_atomic.c \
-	$(LOCAL_PATH)/src/atomic/SDL_spinlock.c.arm \
+	$(LOCAL_PATH)/src/atomic/SDL_spinlock.c \
 	$(wildcard $(LOCAL_PATH)/src/core/android/*.c) \
 	$(wildcard $(LOCAL_PATH)/src/cpuinfo/*.c) \
 	$(wildcard $(LOCAL_PATH)/src/dynapi/*.c) \
 	$(wildcard $(LOCAL_PATH)/src/events/*.c) \
 	$(wildcard $(LOCAL_PATH)/src/file/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/filesystem/android/*.c) \
 	$(wildcard $(LOCAL_PATH)/src/haptic/*.c) \
 	$(wildcard $(LOCAL_PATH)/src/haptic/dummy/*.c) \
 	$(wildcard $(LOCAL_PATH)/src/joystick/*.c) \
@@ -34,7 +35,6 @@ LOCAL_SRC_FILES := \
 	$(wildcard $(LOCAL_PATH)/src/loadso/dlopen/*.c) \
 	$(wildcard $(LOCAL_PATH)/src/power/*.c) \
 	$(wildcard $(LOCAL_PATH)/src/power/android/*.c) \
-	$(wildcard $(LOCAL_PATH)/src/filesystem/dummy/*.c) \
 	$(wildcard $(LOCAL_PATH)/src/render/*.c) \
 	$(wildcard $(LOCAL_PATH)/src/render/*/*.c) \
 	$(wildcard $(LOCAL_PATH)/src/stdlib/*.c) \
@@ -65,7 +65,7 @@ APP_SRC := $(LOCAL_PATH)/..
 
 LOCAL_SRC_FILES += $(LOCAL_PATH)/src/main/android/SDL_android_main.c
 
-LOCAL_LDLIBS := 
+LOCAL_LDLIBS :=
 LOCAL_EXPORT_LDLIBS := -Wl,--undefined=Java_org_libsdl_app_SDLActivity_nativeInit -ldl -lGLESv1_CM -lGLESv2 -llog -landroid
 
 include $(BUILD_STATIC_LIBRARY)
