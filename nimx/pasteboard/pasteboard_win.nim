453ed1c95837ac774ffd6cf29b79a180c843004d
tormund <tormund@zeoalliance.com>
2016-07-28 17:38:53 +0300

    cleanup

--- nimx/pasteboard/pasteboard_win.nim
+++ nimx/pasteboard/pasteboard_win.nim
@@ -3,7 +3,10 @@ export abstract_pasteboard
 import winlean
 import os
 
-{.pragma: winApi, stdcall, nodecl, dynlib: "user32", header:"windows.h".}
+{.pragma: user32, stdcall, dynlib: "user32" .}
+{.pragma: kernel32, stdcall, dynlib: "kernel32" .}
+
+const UniSuffix = when useWinUnicode: "W" else: "A"
 
 type
     LPVOID = pointer
@@ -17,67 +20,67 @@ const
 # BOOL WINAPI OpenClipboard(
 #   _In_opt_ HWND hWndNewOwner
 # );
-proc openClipboard(hwnd: Handle = 0): WINBOOL {.winApi, importc: "OpenClipboard".}
+proc openClipboard(hwnd: Handle = 0): WINBOOL {.user32, importc: "OpenClipboard".}
 
 # BOOL WINAPI CloseClipboard(void);
-proc closeClipboard(): WINBOOL {.winApi, importc: "CloseClipboard".}
-
-# LPVOID WINAPI GlobalLock(
-#   _In_ HGLOBAL hMem
-# );
-proc globalLock(hMem: Handle): LPVOID {.winApi, importc: "GlobalLock".}
-
-# BOOL WINAPI GlobalUnlock(
-#   _In_ HGLOBAL hMem
-# );
-proc globalUnlock(hMem: Handle): WINBOOL {.winApi, importc: "GlobalUnlock".}
+proc closeClipboard(): WINBOOL {.user32, importc: "CloseClipboard".}
 
 # HANDLE WINAPI GetClipboardData(
 #   _In_ UINT uFormat
 # );
-proc getClipboardData(uFormat: UINT):Handle {.winApi, importc: "GetClipboardData".}
+proc getClipboardData(uFormat: UINT):Handle {.user32, importc: "GetClipboardData".}
 
 # HANDLE WINAPI SetClipboardData(
 #   _In_     UINT   uFormat,
 #   _In_opt_ HANDLE hMem
 # );
-proc setClipboardData(uFormat: UINT, hMem: Handle = 0): Handle {.winApi, importc: "SetClipboardData".}
+proc setClipboardData(uFormat: UINT, hMem: Handle = 0): Handle {.user32, importc: "SetClipboardData".}
 
 # BOOL WINAPI EmptyClipboard(void);
-proc emptyClipboard(): WINBOOL {.winApi, importc: "EmptyClipboard".}
+proc emptyClipboard(): WINBOOL {.user32, importc: "EmptyClipboard".}
 
 # BOOL WINAPI IsClipboardFormatAvailable(
 #   _In_ UINT format
 # );
-proc isClipboardFormatAvailable(format: UINT): WINBOOL {.winApi, importc: "IsClipboardFormatAvailable".}
-
-# HGLOBAL WINAPI GlobalAlloc(
-#   _In_ UINT   uFlags,
-#   _In_ SIZE_T dwBytes
-# );
-proc globalAlloc(uFlags: UINT, dwBytes: csize): Handle {.winApi, importc: "GlobalAlloc".}
-
-# HGLOBAL WINAPI GlobalFree(
-#   _In_ HGLOBAL hMem
-# );
-proc globalFree(hMem: Handle): Handle {.winApi, importc: "GlobalFree".}
-
-# SIZE_T WINAPI GlobalSize(
-#   _In_ HGLOBAL hMem
-# );
-proc globalSize(hMem: Handle): csize {.winApi, importc: "GlobalSize".}
+proc isClipboardFormatAvailable(format: UINT): WINBOOL {.user32, importc: "IsClipboardFormatAvailable".}
 
 # UINT WINAPI RegisterClipboardFormat(
 #   _In_ LPCTSTR lpszFormat
 # );
-proc registerClipboardFormat(lpszFormat: pointer): UINT {.winApi, importc: "RegisterClipboardFormat".}
+proc registerClipboardFormat(lpszFormat: pointer): UINT {.user32, importc: "RegisterClipboardFormat" & UniSuffix.}
 
 # int WINAPI GetClipboardFormatName(
 #   _In_  UINT   format,
 #   _Out_ LPTSTR lpszFormatName,
 #   _In_  int    cchMaxCount
 # );
-proc getClipboardFormatName(uFormat: UINT, lpszFormatName: pointer, cchMaxCount: int32): int32 {.winApi, importc: "GetClipboardFormatName".}
+proc getClipboardFormatName(uFormat: UINT, lpszFormatName: pointer, cchMaxCount: int32): int32 {.user32, importc: "GetClipboardFormatName" & UniSuffix.}
+
+# HGLOBAL WINAPI GlobalAlloc(
+#   _In_ UINT   uFlags,
+#   _In_ SIZE_T dwBytes
+# );
+proc globalAlloc(uFlags: UINT, dwBytes: csize): Handle {.kernel32, importc: "GlobalAlloc".}
+
+# HGLOBAL WINAPI GlobalFree(
+#   _In_ HGLOBAL hMem
+# );
+proc globalFree(hMem: Handle): Handle {.kernel32, importc: "GlobalFree".}
+
+# SIZE_T WINAPI GlobalSize(
+#   _In_ HGLOBAL hMem
+# );
+proc globalSize(hMem: Handle): csize {.kernel32, importc: "GlobalSize".}
+
+# LPVOID WINAPI GlobalLock(
+#   _In_ HGLOBAL hMem
+# );
+proc globalLock(hMem: Handle): LPVOID {.kernel32, importc: "GlobalLock".}
+
+# BOOL WINAPI GlobalUnlock(
+#   _In_ HGLOBAL hMem
+# );
+proc globalUnlock(hMem: Handle): WINBOOL {.kernel32, importc: "GlobalUnlock".}
 
 proc `*`(b: SomeOrdinal): bool = result = b != 0
 
