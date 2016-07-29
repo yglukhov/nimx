497ffc2c8ea853fede6158cf917912ef53873206
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2016-07-29 17:13:55 +0300

    Fixed last line in grid

--- nimx/editor/grid_drawing.nim
+++ nimx/editor/grid_drawing.nim
@@ -7,7 +7,7 @@ proc drawGrid*(bounds: Rect, gridSize: Size, shift = zeroSize) =
     var r = newRect(0, 0, 1, bounds.height)
     if gridSize.width > 0:
         let n = int((bounds.width - bounds.x - shift.width) / gridSize.width)
-        for x in 0 ..< n:
+        for x in 0 .. n:
             r.origin.x = bounds.x + shift.width + Coord(x) * gridSize.width
             c.drawRect(r)
     if gridSize.height > 0:
@@ -15,6 +15,6 @@ proc drawGrid*(bounds: Rect, gridSize: Size, shift = zeroSize) =
         r.size.width = bounds.width
         r.size.height = 1
         let n = int((bounds.height - bounds.y - shift.height) / gridSize.height)
-        for y in 0 ..< n:
+        for y in 0 .. n:
             r.origin.y = bounds.y + shift.height + Coord(y) * gridSize.height
             c.drawRect(r)
