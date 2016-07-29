f67a15b0e277a3de903d444d985b2dd2e98d8d02
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2016-07-29 14:53:48 +0300

    Fixed precision issue in grid drawing

--- nimx/editor/grid_drawing.nim
+++ nimx/editor/grid_drawing.nim
@@ -1,18 +1,20 @@
 import nimx.context
 import nimx.types
 
-proc drawGrid*(bounds: Rect, gridSize: Size) =
+proc drawGrid*(bounds: Rect, gridSize: Size, shift = zeroSize) =
     let c = currentContext()
     c.strokeWidth = 0
     var r = newRect(0, 0, 1, bounds.height)
     if gridSize.width > 0:
-        for x in countup(0, int(bounds.width), int(gridSize.width)):
-            r.origin.x = Coord(x)
+        let n = int((bounds.width - bounds.x - shift.width) / gridSize.width)
+        for x in 0 ..< n:
+            r.origin.x = bounds.x + shift.width + Coord(x) * gridSize.width
             c.drawRect(r)
     if gridSize.height > 0:
         r.origin.x = 0
         r.size.width = bounds.width
         r.size.height = 1
-        for y in countup(0, int(bounds.height), int(gridSize.height)):
-            r.origin.y = Coord(y)
+        let n = int((bounds.height - bounds.y - shift.height) / gridSize.height)
+        for y in 0 ..< n:
+            r.origin.y = bounds.y + shift.height + Coord(y) * gridSize.height
             c.drawRect(r)
