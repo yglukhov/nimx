92b89ecb97a4d160a4cc5745904dea1f43ae0bef
rrenderr <rrenderr@gmail.com>
2016-08-24 14:08:42 +0300

    Image Preview (#155)
    * - fix nil context  on js
    
    * - added transformPoint and transformDirection for matrixes
    - fix tryGetTranslationFromModel
    
    * - remove transformPoint because Matrix4.multiply
    
    * - fix tryGetScaleRotationFromModel to Row-major
    
    * - fix lookAt
    
    * - added subpixel text rendering
    
    * - now popup_button return selected item
    
    * - modified text render for perspective camera
    
    * - added log texture memory
    - added proc normalized*(v: Vector3): Vector3
    
    * - fix glyph spacing
    - fix dump bitmaps
    - added drawTextBase (draw text without any material)
    
    * Color math
    
    * - added listener functionality for events
    - added sample for listener functionality
    - added onFocusChange functionality for desktops
    - added clearDepthStencil
    
    * - according git comments
    
    * - fix fonts
    
    * - to previous
    
    * - remove margin from chars map
    
    * - added LINE_LOOP
    - added proc perspective*(dest: var Matrix4, left, right, bottom, top, nearr, farr: Coord)
    
    * - calculate perspective using frustum
    
    * - LINE_LOOP for JS
    
    * - added image preview
    - fix touch events for JS

---
+++ nimx/image_preview.nim
@@ -0,0 +1,105 @@
+import math
+
+import nimx.font
+import nimx.image
+import nimx.button
+import nimx.view
+import nimx.event
+import nimx.view_event_handling_new
+import nimx.context
+import nimx.types
+
+const titleSize = 20.0
+const bottomSize = 30.0
+const maxSize = 768.0
+const minSize = 128.0
+
+type ImagePreview* = ref object of View
+    image*: Image
+    title*: string
+    contentView*: View
+    imgScale*: float
+
+method init*(v: ImagePreview, r: Rect) =
+    procCall v.View.init(r)
+    v.backgroundColor = newColor(0.2, 0.2, 0.2, 1.0)
+    v.title = "Image Preview"
+
+    let closeBttn = newButton(v, newPoint(r.width - 16.0 - 1.0, 1.0), newSize(16, 16), "X")
+    closeBttn.onAction do():
+        v.removeFromSuperview()
+
+proc newImagePreview*(r: Rect, img: Image): ImagePreview =
+    let maxLen = max(img.size.width, img.size.height)
+    var scale = 1.0
+    if maxLen > maxSize:
+        scale = maxSize / maxLen
+
+    var content = newSize(img.size.width * scale, img.size.height * scale)
+    if img.size.width < minSize:
+        content.width = minSize
+    if img.size.height < minSize:
+        content.height = minSize
+
+    var viewRect: Rect
+    viewRect.size.width = content.width + 2.0
+    viewRect.size.height = content.height + titleSize + 1.0 + 30.0
+
+    result.new()
+    result.init(viewRect)
+    result.image = img
+    result.imgScale = scale
+
+method draw(v: ImagePreview, r: Rect) =
+    procCall v.View.draw(r)
+    let c = currentContext()
+    let f = systemFontOfSize(14.0)
+    var titleRect: Rect
+    titleRect.size.width = r.width
+    titleRect.size.height = titleSize
+    c.fillColor = newColor(0.2, 0.2, 0.2)
+    c.drawRect(titleRect)
+
+    var contentRect: Rect
+    contentRect.origin.x = 1.0
+    contentRect.origin.y = titleSize
+    contentRect.size.width = r.width - 2.0
+    contentRect.size.height = r.height - titleSize - bottomSize - 1.0
+    c.fillColor = newColor(0.5, 0.5, 0.5)
+    c.drawRect(contentRect)
+
+    c.fillColor = newColor(0.9, 0.9, 0.9)
+    c.drawText(f, newPoint(5, 1), v.title)
+
+    # Draw Image
+    var imageRect: Rect
+    imageRect.origin.x = 1.0
+    imageRect.origin.y = titleSize
+    imageRect.size.width = v.image.size.width * v.imgScale
+    imageRect.size.height = v.image.size.height * v.imgScale
+    c.drawImage(v.image, imageRect)
+
+    # Draw Info
+    let sizeInfo = "Size: " & $v.image.size.width & " x " & $v.image.size.height
+    c.drawText(f, newPoint(5, r.height - bottomSize / 2.0), sizeInfo)
+
+    var pathInfo = "Path: nil"
+    if not v.image.filePath.isNil:
+        pathInfo = "Path: " & $v.image.filePath
+    c.drawText(f, newPoint(5, r.height - bottomSize), pathInfo)
+
+method onTouchEv*(v: ImagePreview, e: var Event) : bool =
+    discard procCall v.View.onTouchEv(e)
+    result = true
+
+proc popupAtPoint*(ip: ImagePreview, v: View, p: Point) =
+    ip.removeFromSuperview()
+    var origin: Point
+    origin = v.convertPointToWindow(p)
+    if origin.x > v.window.bounds.size.width / 2.0:
+        origin.x -= ip.image.size.width * ip.imgScale
+    else:
+        origin.x += 50.0
+    origin.y = 35.0
+    ip.setFrameOrigin(origin)
+    v.window.addSubview(ip)
