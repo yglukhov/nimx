1de39a14ba9ff17981294d191d62b77dffd20853
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2016-08-31 18:10:12 +0300

    fixup

--- nimx/private/text_drawing.nim
+++ nimx/private/text_drawing.nim
@@ -123,6 +123,11 @@ void main() {
 }
 """,
 """
+#ifdef GL_ES
+#extension GL_OES_standard_derivatives : enable
+precision mediump float;
+#endif
+
 uniform sampler2D texUnit;
 uniform vec4 fillColor;
 uniform float preScale;
