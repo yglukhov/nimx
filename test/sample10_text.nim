d84c74d0e015394a46080be6765c33a49809a6ae
Андрей Гутыря <rrenderr@zeoalliance.com>
2016-09-22 16:50:43 +0300

    - text soft shadow

--- test/sample10_text.nim
+++ test/sample10_text.nim
@@ -18,7 +18,13 @@ type TextView = ref object of View
 
 type TextSampleView = ref object of View
 
-const textSample = """Nim is statically typed, with a simple syntax. It supports compile-time metaprogramming features such as syntactic macros and term rewriting macros. Term rewriting macros enable library implementations of common data structures such as bignums and matrixes to be implemented with an efficiency as if they would have been builtin language facilities. Iterators are supported and can be used as first class entities in the language as can functions, these features allow for functional programming to be used. Object-oriented programming is supported by inheritance and multiple dispatch. Functions can be generic and can also be overloaded, generics are further enhanced by the support for type classes. Operator overloading is also supported. Nim includes automatic garbage collection based on deferred reference counting with cycle detection. Andrew Binstock (editor-in-chief of Dr. Dobb's) says Nim (formerly known as Nimrod) "presents a most original design that straddles Pascal and Python and compiles to C code or JavaScript." """
+const textSample = """Nim is statically typed, with a simple syntax. It supports compile-time metaprogramming features such as syntactic macros and term rewriting macros.
+    Term rewriting macros enable library implementations of common data structures such as bignums and matrixes to be implemented with an efficiency as if they would have been builtin language facilities.
+    Iterators are supported and can be used as first class entities in the language as can functions, these features allow for functional programming to be used.
+    Object-oriented programming is supported by inheritance and multiple dispatch. Functions can be generic and can also be overloaded, generics are further enhanced by the support for type classes.
+    Operator overloading is also supported. Nim includes automatic garbage collection based on deferred reference counting with cycle detection.
+    Andrew Binstock (editor-in-chief of Dr. Dobb's) says Nim (formerly known as Nimrod) "presents a most original design that straddles Pascal and Python and compiles to C code or JavaScript.
+    And realistic SoftSadow :)" """
 
 #const textSample = """Hello"""
 
@@ -48,10 +54,14 @@ method init(v: TextSampleView, r: Rect) =
 
     for a, b in tv.text.rangesOfSubstring("programming"):
         tv.formattedText.setTextColorInRange(a, b, newColor(1, 0, 0))
-        tv.formattedText.setShadowInRange(a, b, newGrayColor(0.5, 0.5), newSize(2, 3))
+        tv.formattedText.setShadowInRange(a, b, newGrayColor(0.5, 0.5), newSize(2, 3), 0.0, 0.0)
 
     for a, b in tv.text.rangesOfSubstring("supported"):
         tv.formattedText.setTextColorInRange(a, b, newColor(0, 0.6, 0))
+
+    for a, b in tv.text.rangesOfSubstring("SoftSadow"):
+        tv.formattedText.setFontInRange(a, b, systemFontOfSize(40))
+        tv.formattedText.setShadowInRange(a, b, newColor(0.0, 0.0, 1.0, 1.0), newSize(2, 3), 5.0, 0.8)
 
     let sv = newScrollView(tv)
     v.addSubview(sv)
