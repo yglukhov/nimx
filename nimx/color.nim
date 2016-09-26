2c9544ef7b33707cea4df516035ed20a59184a5e
rrenderr <rrenderr@gmail.com>
2016-09-26 18:11:57 +0300

    expanding view (#163)
    * - fix nil context  on js
    
    * - added transformPoint and transformDirection for matrixes
    - fix tryGetTranslationFromModel
    
    * - remove transformPoint because Matrix4.multiply
    
    * - fix tryGetScaleRotationFromModel to Row-major
    
    * - fix lookAt
    
    * - added subpixel text rendering
    
    * - now popup_button return selected item
    
    * - modified text render for perspective camera
    
    * - added log texture memory
    - added proc normalized*(v: Vector3): Vector3
    
    * - fix glyph spacing
    - fix dump bitmaps
    - added drawTextBase (draw text without any material)
    
    * Color math
    
    * - added listener functionality for events
    - added sample for listener functionality
    - added onFocusChange functionality for desktops
    - added clearDepthStencil
    
    * - according git comments
    
    * - fix fonts
    
    * - to previous
    
    * - remove margin from chars map
    
    * - added LINE_LOOP
    - added proc perspective*(dest: var Matrix4, left, right, bottom, top, nearr, farr: Coord)
    
    * - calculate perspective using frustum
    
    * - LINE_LOOP for JS
    
    * - added image preview
    - fix touch events for JS
    
    * - fix emscripten touches
    - new constant for subpixels text
    
    * - added expanding view
    - added color.nim - for standard colors
    - TODO stack_view is a same VerticalLayout

---
+++ nimx/color.nim
@@ -0,0 +1,6 @@
+import math
+import types
+
+proc titleBarColor*(): Color = newColor(0.2, 0.2, 0.2)
+proc contentViewColor*(): Color = newColor(0.5, 0.5, 0.5)
+proc titleTextColor*(): Color = newColor(0.9, 0.9, 0.9)
