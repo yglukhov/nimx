25240d14e19e25f581bf5f5ef9df8966c72213da
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2017-02-08 11:03:39 +0200

    Moved window implementations to private

--- nimx/window.nim
+++ nimx/window.nim
@@ -1,14 +1,14 @@
 # Platform specific implementations follow
 import abstract_window
 when defined(js):
-    import js_canvas_window
+    import private.windows.js_canvas_window
     export startAnimation
 elif defined(emscripten):
-    import emscripten_window
+    import private.windows.emscripten_window
 elif defined(macosx) and not defined(ios) and defined(nimxAvoidSDL):
-    import appkit_window
+    import private.windows.appkit_window
 else:
-    import sdl_window
+    import private.windows.sdl_window
     export runUntilQuit
 
 export runApplication
