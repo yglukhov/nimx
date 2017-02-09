3b347e1c3c424d01d5d93ea6e134dab51533b947
tormund <tormund@zeoalliance.com>
2017-02-09 15:37:32 +0200

    view render to image

---
+++ nimx/view_render_to_image.nim
@@ -0,0 +1,14 @@
+import nimx.view
+import nimx.render_to_image
+import nimx.image
+import nimx.types
+
+proc renderToImage*(v: View, image: SelfContainedImage)=
+    assert v.bounds.size == image.size
+    image.draw do():
+        v.draw(newRect(0, 0, v.bounds.width, v.bounds.height))
+
+proc screenShot*(v: View):SelfContainedImage=
+    var image = imageWithSize(v.bounds.size)
+    v.renderToImage(image)
+    result = image
