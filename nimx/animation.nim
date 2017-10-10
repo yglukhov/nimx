bb60efeb6c190b417927cf4a97c8bb6991fda176
Voron <Voron@zeoalliance.com>
2017-10-10 16:02:29 +0300

    change name

--- nimx/animation.nim
+++ nimx/animation.nim
@@ -20,7 +20,7 @@ type CancelBehavior* = enum
 type TimingFunction = proc(time: float): float
 type AnimationFunction = proc(progress: float)
 
-const MIN_DURATION:float = 0.0
+const MIN_LOOP_DURATION:float = 0.0
 
 type ProgressHandler = object
     handler: proc()
@@ -149,9 +149,8 @@ method onProgress*(a: Animation, p: float) {.base.} =
 
 proc loopProgress(a: Animation, t: float): float=
     var duration = t - a.startTime
-    #doAssert(duration > -0.0001, $duration)
-    if duration < MIN_DURATION:
-        duration = MIN_DURATION
+    if duration < MIN_LOOP_DURATION:
+        duration = MIN_LOOP_DURATION
 
     a.curLoop = a.currentLoopForTotalDuration(duration)
     result = (duration mod a.loopDuration) / a.loopDuration
