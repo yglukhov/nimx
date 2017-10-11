ac7f724021facc82b992b6ce7f4a3b87091cbacf
Андрей Гутыря <rrenderr@zeoalliance.com>
2017-10-11 13:24:04 +0300

    - get only drop view

--- nimx/drag_and_drop.nim
+++ nimx/drag_and_drop.nim
@@ -53,21 +53,30 @@ method onDrop*(dd: DragAndDrop, i: DraggedItem) {.base.} = discard
 method onDragEnter*(dd: DragAndDrop, i: DraggedItem) {.base.} = discard
 method onDragExit*(dd: DragAndDrop, i: DraggedItem) {.base.} = discard
 
+# method onDrag*(v: DragAndDrop, i: DraggedItem) {.base.} = discard
+# method onDrop*(dd: DragAndDrop, i: DraggedItem) {.base.} = discard
+# method onDragEnter*(dd: DragAndDrop, i: DraggedItem) {.base.} = discard
+# method onDragExit*(dd: DragAndDrop, i: DraggedItem) {.base.} = discard
 
-proc findSubviewAtPointAux*(v: View, p: Point): View =
+proc findSubviewAtPointAux*(v: View, p: Point, target: var View): View =
     for i in countdown(v.subviews.len - 1, 0):
         let s = v.subviews[i]
         var pp = s.convertPointFromParent(p)
         if pp.inRect(s.bounds):
-            result = s.findSubviewAtPointAux(pp)
+            if not v.dragAndDropDelegate.isNil:
+                target = v
+            result = s.findSubviewAtPointAux(pp, target)
             if not result.isNil:
                 break
 
     if result.isNil:
         result = v
+        if not result.dragAndDropDelegate.isNil:
+            target = result
+
 
 proc findSubviewAtPoint*(v: View, p: Point): View =
-    result = v.findSubviewAtPointAux(p)
+    discard v.findSubviewAtPointAux(p, result)
     if result == v: result = nil
 
 
