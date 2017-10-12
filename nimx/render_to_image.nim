169d611013042a3144166f074882d510aa361194
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2017-10-12 17:26:51 +0300

    Image refactoring stage 2. New interface for render to image.

--- nimx/render_to_image.nim
+++ nimx/render_to_image.nim
@@ -1,108 +1,178 @@
-import image
-import portable_gl
-import context
+import image, types, context, portable_gl
 import math
 import opengl
 
-type GlFrameState* = tuple[
-    clearColor: array[4, GLfloat],
-    viewportSize: array[4, GLint],
-    renderbuffer: RenderbufferRef,
-    framebuffer: FramebufferRef,
+type GlFrameState* = tuple
+    clearColor: array[4, GLfloat]
+    viewportSize: array[4, GLint]
+    framebuffer: FramebufferRef
     bStencil: bool
-]
+    rt: ImageRenderTarget
 
-proc bindFramebuffer*(gl: GL, i: SelfContainedImage, makeDepthAndStencil: bool = true) =
-    if i.mRenderTarget.isNil:
-        i.mRenderTarget = newImageRenderTarget()
-    let rt = i.mRenderTarget
-    if rt.framebuffer.isEmpty:
-        var texCoords : array[4, GLfloat]
-        var texture = i.getTextureQuad(gl, texCoords)
+proc dispose*(r: ImageRenderTarget) =
+    let gl = sharedGL()
+    if r.framebuffer != invalidFrameBuffer:
+        gl.deleteFramebuffer(r.framebuffer)
+        r.framebuffer = invalidFrameBuffer
+    if r.depthbuffer != invalidRenderbuffer:
+        gl.deleteRenderbuffer(r.depthbuffer)
+        r.depthbuffer = invalidRenderbuffer
+    if r.stencilbuffer != invalidRenderbuffer:
+        gl.deleteRenderbuffer(r.stencilbuffer)
+        r.stencilbuffer = invalidRenderbuffer
 
-        if texture.isEmpty:
-            texture = gl.createTexture()
-            i.texture = texture
-
-        rt.framebuffer = gl.createFramebuffer()
-        gl.bindFramebuffer(gl.FRAMEBUFFER, rt.framebuffer)
-        gl.bindTexture(gl.TEXTURE_2D, texture)
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
-
-        let texWidth = if isPowerOfTwo(i.size.width.int): i.size.width.int else: nextPowerOfTwo(i.size.width.int)
-        let texHeight = if isPowerOfTwo(i.size.height.int): i.size.height.int else: nextPowerOfTwo(i.size.height.int)
-
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA.GLint, texWidth.GLsizei, texHeight.GLsizei, 0, gl.RGBA, gl.UNSIGNED_BYTE, nil)
-        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0)
-        if makeDepthAndStencil:
-            let depthBuffer = gl.createRenderbuffer()
-            gl.bindRenderbuffer(gl.RENDERBUFFER, depthBuffer)
-
-            let depthStencilFormat = when defined(js) or defined(emscripten): gl.DEPTH_STENCIL else: gl.DEPTH24_STENCIL8
-
-            # TODO: The following is a very dirty fix for ES 2.0 devices that don't support DEPTH_STENCIL_ATTACHMENT
-            # for such devices we provide only DEPTH attachment, which can result in artifacts when drawing relies
-            # on stencil buffer. This should be fixed with refactoring of render-to-texture system.
-            when NoAutoGLerrorCheck:
-                discard gl.getError()
-                gl.renderbufferStorage(gl.RENDERBUFFER, depthStencilFormat, texWidth.GLsizei, texHeight.GLsizei)
-                if gl.getError() == 0.GLenum:
-                    gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_STENCIL_ATTACHMENT, gl.RENDERBUFFER, depthBuffer)
-                else:
-                    gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, texWidth.GLsizei, texHeight.GLsizei)
-                    gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, depthBuffer)
-            else:
-                try:
-                    gl.renderbufferStorage(gl.RENDERBUFFER, depthStencilFormat, texWidth.GLsizei, texHeight.GLsizei)
-                    gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_STENCIL_ATTACHMENT, gl.RENDERBUFFER, depthBuffer)
-                except:
-                    gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, texWidth.GLsizei, texHeight.GLsizei)
-                    gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, depthBuffer)
-
-            rt.renderbuffer = depthBuffer
+proc newImageRenderTarget*(needsDepthStencil: bool = true): ImageRenderTarget {.inline.} =
+    when defined(js):
+        result.new()
     else:
+        result.new(dispose)
+    result.needsDepthStencil = needsDepthStencil
+
+proc init(rt: ImageRenderTarget, texWidth, texHeight: int16) =
+    let gl = sharedGL()
+    rt.texWidth = texWidth
+    rt.texHeight = texHeight
+    rt.framebuffer = gl.createFramebuffer()
+
+    if rt.needsDepthStencil:
+        let oldFramebuffer = gl.boundFramebuffer()
         gl.bindFramebuffer(gl.FRAMEBUFFER, rt.framebuffer)
 
-proc getGlFrameState*(gfs: var GlFrameState) =
-    let gl = sharedGL()
-    gfs.framebuffer = gl.boundFramebuffer()
-    gfs.viewportSize = gl.getViewport()
-    gfs.renderbuffer = gl.boundRenderbuffer()
-    gfs.bStencil = gl.getParamb(gl.STENCIL_TEST)
-    gl.getClearColor(gfs.clearColor)
+        let depthBuffer = gl.createRenderbuffer()
+        gl.bindRenderbuffer(gl.RENDERBUFFER, depthBuffer)
+        let depthStencilFormat = when defined(js) or defined(emscripten): gl.DEPTH_STENCIL else: gl.DEPTH24_STENCIL8
 
-proc setImageGlFrameState*(sci: SelfContainedImage) =
+        # The following tries to use DEPTH_STENCIL_ATTACHMENT, but it may fail on some devices,
+        # so for those we're creating a separate stencil buffer.
+        var needsStencil = false
+        when NoAutoGLerrorCheck:
+            discard gl.getError()
+            gl.renderbufferStorage(gl.RENDERBUFFER, depthStencilFormat, texWidth, texHeight)
+            if gl.getError() == 0.GLenum:
+                gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_STENCIL_ATTACHMENT, gl.RENDERBUFFER, depthBuffer)
+            else:
+                gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, texWidth, texHeight)
+                gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, depthBuffer)
+                needsStencil = true
+        else:
+            try:
+                gl.renderbufferStorage(gl.RENDERBUFFER, depthStencilFormat, texWidth, texHeight)
+                gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_STENCIL_ATTACHMENT, gl.RENDERBUFFER, depthBuffer)
+            except:
+                gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, texWidth, texHeight)
+                gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, depthBuffer)
+                needsStencil = true
+
+        rt.depthbuffer = depthBuffer
+
+        if needsStencil:
+            let stencilBuffer = gl.createRenderbuffer()
+            gl.bindRenderbuffer(gl.RENDERBUFFER, stencilBuffer)
+            gl.renderbufferStorage(gl.RENDERBUFFER, gl.STENCIL_INDEX8, texWidth, texHeight)
+            gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.STENCIL_ATTACHMENT, gl.RENDERBUFFER, stencilBuffer)
+            rt.stencilbuffer = stencilBuffer
+
+        gl.bindFramebuffer(gl.FRAMEBUFFER, oldFramebuffer)
+
+proc resize(rt: ImageRenderTarget, texWidth, texHeight: int16) =
     let gl = sharedGL()
-    gl.bindFramebuffer(sci)
-    gl.viewport(0, 0, sci.size.width.GLsizei, sci.size.height.GLsizei)
+    rt.texWidth = max(rt.texWidth, texWidth)
+    rt.texHeight = max(rt.texHeight, texHeight)
+
+    if rt.depthbuffer != invalidRenderbuffer:
+        let depthStencilFormat = if rt.stencilbuffer == invalidRenderbuffer:
+                when defined(js) or defined(emscripten): gl.DEPTH_STENCIL else: gl.DEPTH24_STENCIL8
+            else:
+                gl.DEPTH_COMPONENT16
+
+        gl.bindRenderbuffer(gl.RENDERBUFFER, rt.depthbuffer)
+        gl.renderbufferStorage(gl.RENDERBUFFER, depthStencilFormat, rt.texWidth, rt.texHeight)
+
+    if rt.stencilBuffer != invalidRenderbuffer:
+        gl.bindRenderbuffer(gl.RENDERBUFFER, rt.stencilBuffer)
+        gl.renderbufferStorage(gl.RENDERBUFFER, gl.STENCIL_INDEX8, rt.texWidth, rt.texHeight)
+
+proc setImage*(rt: ImageRenderTarget, i: SelfContainedImage) =
+    assert(i.texWidth != 0 and i.texHeight != 0)
+
+    let gl = sharedGL()
+    var texCoords: array[4, GLfloat]
+    var texture = i.getTextureQuad(gl, texCoords)
+    if texture.isEmpty:
+        texture = gl.createTexture()
+        i.texture = texture
+        gl.bindTexture(gl.TEXTURE_2D, texture)
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA.GLint, i.texWidth, i.texHeight, 0, gl.RGBA, gl.UNSIGNED_BYTE, nil)
+
+    if rt.framebuffer.isEmpty:
+        rt.init(i.texWidth, i.texHeight)
+    elif rt.texWidth < i.texWidth or rt.texHeight < i.texHeight:
+        rt.resize(i.texWidth, i.texHeight)
+
+    let br = i.backingRect()
+
+    rt.vpX = br.x.GLint
+    rt.vpY = br.y.GLint
+    rt.vpW = br.width.GLsizei
+    rt.vpH = br.height.GLsizei
+
+    let oldFramebuffer = gl.boundFramebuffer()
+
+    gl.bindFramebuffer(gl.FRAMEBUFFER, rt.framebuffer)
+    gl.bindTexture(gl.TEXTURE_2D, texture)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0)
+
+    gl.bindFramebuffer(gl.FRAMEBUFFER, oldFramebuffer)
+
+proc beginDraw*(t: ImageRenderTarget, state: var GlFrameState) =
+    assert(t.vpW != 0 and t.vpH != 0)
+
+    let gl = sharedGL()
+    state.framebuffer = gl.boundFramebuffer()
+    state.viewportSize = gl.getViewport()
+    state.bStencil = gl.getParamb(gl.STENCIL_TEST)
+    gl.getClearColor(state.clearColor)
+
+    gl.bindFramebuffer(gl.FRAMEBUFFER, t.framebuffer)
+    gl.viewport(t.vpX, t.vpY, t.vpW, t.vpH)
     gl.stencilMask(0xFF) # Android requires setting stencil mask to clear
     gl.clearColor(0, 0, 0, 0)
     gl.clear(gl.COLOR_BUFFER_BIT or gl.DEPTH_BUFFER_BIT or gl.STENCIL_BUFFER_BIT)
     gl.stencilMask(0x00) # Android requires setting stencil mask to clear
     gl.disable(gl.STENCIL_TEST)
 
-proc restoreGlFrameState*(gfs: var GlFrameState) =
+proc endDraw*(t: ImageRenderTarget, state: var GlFrameState) =
     let gl = sharedGL()
-    if gfs.bStencil:
+    if state.bStencil:
         gl.enable(gl.STENCIL_TEST)
-    gl.clearColor(gfs.clearColor[0], gfs.clearColor[1], gfs.clearColor[2], gfs.clearColor[3])
-    gl.viewport(gfs.viewportSize)
-    gl.bindRenderbuffer(gl.RENDERBUFFER, gfs.renderbuffer)
-    gl.bindFramebuffer(gl.FRAMEBUFFER, gfs.framebuffer)
+    gl.clearColor(state.clearColor[0], state.clearColor[1], state.clearColor[2], state.clearColor[3])
+    gl.viewport(state.viewportSize)
+    gl.bindFramebuffer(gl.FRAMEBUFFER, state.framebuffer)
 
-proc beginDraw*(sci: SelfContainedImage, gfs: var GlFrameState) =
-    getGlFrameState(gfs)
-    sci.setImageGlFrameState()
+proc beginDraw*(sci: SelfContainedImage, gfs: var GlFrameState) {.deprecated.} =
+    var rt = sci.mRenderTarget
+    if rt.isNil:
+        rt = newImageRenderTarget()
+        sci.mRenderTarget = rt
+    rt.setImage(sci)
+    rt.beginDraw(gfs)
 
-proc endDraw*(sci: SelfContainedImage, gfs: var GlFrameState) =
-    restoreGlFrameState(gfs)
+proc endDraw*(sci: SelfContainedImage, gfs: var GlFrameState) {.deprecated.} =
+    assert(not sci.mRenderTarget.isNil)
+    sci.mRenderTarget.endDraw(gfs)
 
 proc draw*(sci: SelfContainedImage, drawProc: proc()) =
     var gfs: GlFrameState
-    beginDraw(sci, gfs)
+    let rt = newImageRenderTarget()
+    rt.setImage(sci)
+    rt.beginDraw(gfs)
+
     currentContext().withTransform ortho(0, sci.size.width, sci.size.height, 0, -1, 1):
         drawProc()
-    endDraw(sci, gfs)
+
+    rt.endDraw(gfs)
+    rt.dispose()
     # OpenGL framebuffer coordinate system is flipped comparing to how we load
     # and handle the rest of images. Compensate for that by flipping texture
     # coords here.
@@ -114,4 +184,3 @@ proc draw*(i: Image, drawProc: proc()) {.deprecated.} =
     if sci.isNil:
         raise newException(Exception, "Not implemented: Can draw only to SelfContainedImage")
     sci.draw(drawProc)
-
