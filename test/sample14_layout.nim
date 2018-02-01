1cda8d31d9a93b8d4cc95933736d92a995c500e7
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2018-02-01 16:21:59 +0200

    More layout samples

--- test/sample14_layout.nim
+++ test/sample14_layout.nim
@@ -1,7 +1,19 @@
 import sample_registry
-import nimx / [ view, window, button, text_field, layout ]
+import nimx / [ view, window, button, text_field, layout, scroll_view, table_view,
+                split_view, context ]
 
 type LayoutSampleView = ref object of View
+
+
+type TestView = ref object of View
+method draw*(v: TestView, r: Rect) =
+    procCall v.View.draw(r)
+    let c = currentContext()
+    c.strokeWidth = 2
+    c.strokeColor = blackColor()
+    let b = v.bounds
+    c.drawLine(b.origin, b.maxCorner)
+    c.drawLine(newPoint(b.maxX, b.minY), newPoint(b.minX, b.maxY))
 
 let red = newColor(1, 0, 0)
 let green = newColor(0, 1, 0)
@@ -68,19 +80,161 @@ proc createLayout2(w: Window) =
             height <= super - 20
             height >= 20
 
+
+proc createLayout3(w: Window) =
+    w.makeLayout:
+        - View:
+            backgroundColor: red
+            x == 5
+            y == 5
+            width == super.width / 2
+            height == super.height / 2
+
+            - View:
+                backgroundColor: green
+                x == super.x + 5
+                y == super.x + 5
+                width == 200
+                height == 200
+
+                - View:
+                    backgroundColor: blue
+                    x == super.x + 5
+                    y == super.y + 5
+                    width == 100
+                    height == 100
+
+        - Button:
+            title: "btn1"
+            onAction:
+                echo "hi btn1"
+            width == 50
+            height == 25
+            # x == 10.0
+            # y == 10.0
+            # right == 10.0
+            # height == super.height - 20
+            centerX == super.centerX
+            centerY == super.centerY
+
+proc createLayout4(w: Window) =
+    w.makeLayout:
+        - SplitView as sv:
+            vertical: true # Comment this line to make me horizontal
+            backgroundColor: blue
+            top == super
+            bottom == super
+            leading == super
+            trailing == super
+
+            - TestView:
+                backgroundColor: red
+                width >= 200
+                height >= 200
+
+            - View:
+                backgroundColor: green
+                width >= 200
+                height >= 200
+
+            - View:
+                backgroundColor: yellow
+                width >= 200
+                height >= 200
+
+
+proc createLayout5(w: Window) =
+    w.makeLayout:
+        - View:
+            backgroundColor: blue
+            leading == super + 10
+            trailing == super - 10
+            top == 100
+            bottom == 300
+
+proc createLayout6(w: Window) =
+    w.makeLayout:
+        - SplitView:
+            leading == super + 10
+            trailing == super - 10
+            top == super + 10
+            bottom == super - 10
+
+            - ScrollView:
+                backgroundColor: blue
+                width >= 200
+                height >= 500
+                - TestView:
+                    backgroundColor: red
+                    width == 200
+                    height == 900
+
+            - TestView:
+                backgroundColor: yellow
+                width >= 100
+
+proc createLayout7(w: Window) =
+    var tableValues = newSeq[string]()
+    for i in 0 .. 50:
+        tableValues.add("Item " & $i)
+
+    w.makeLayout:
+        - SplitView:
+            leading == super + 10
+            trailing == super - 10
+            top == super + 10
+            bottom == super - 10
+
+            - ScrollView:
+                backgroundColor: blue
+                width >= 200
+                - TableView as tv:
+                    width == 200
+                    backgroundColor: red
+                    numberOfRows do() -> int:
+                        tableValues.len
+
+                    createCell do() -> TableViewCell:
+                        result = TableViewCell.new(zeroRect)
+                        result.makeLayout:
+                            top == super
+                            bottom == super
+
+                            - Label:
+                                leading == super
+                                trailing == super
+                                top == super
+                                bottom == super
+                                width == 200
+
+                    configureCell do(c: TableViewCell):
+                        Label(c.subviews[0]).text = tableValues[c.row]
+
+            - TestView:
+                backgroundColor: yellow
+
+    tv.reloadData()
+
+
 method init(v: LayoutSampleView, r: Rect) =
     procCall v.View.init(r)
     var by = 5'f32
     template reg(name: string, t: proc(w: Window)) =
-        let b = Button.new(newRect(5, by, 100, 25))
+        let b = Button.new(newRect(5, by, 150, 25))
         b.title = name
         by += 30
         b.onAction do():
             let wnd = newWindow(newRect(80, 80, 800, 600))
+            wnd.title = name
             t(wnd)
         v.addSubview(b)
 
     reg "Hello world", createLayout1
     reg "Some layout", createLayout2
+    reg "Some layout 3", createLayout3
+    reg "Some layout 4", createLayout4
+    reg "Some layout 5", createLayout5
+    reg "SplitView", createLayout6
+    reg "Table in SplitView", createLayout7
 
 registerSample(LayoutSampleView, "Layout")
