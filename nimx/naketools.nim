c69d23a58257c01178ad57a497df50b4c092b8a0
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2018-05-27 19:40:35 +0300

    Fixed compilation error

--- nimx/naketools.nim
+++ nimx/naketools.nim
@@ -180,7 +180,7 @@ proc findEnvPaths(b: Builder) =
         if sdlHome.len == 0: error_msg &= getEnvErrorMsg("SDL_HOME")
 
         if error_msg.len > 0:
-            raiseOSError(error_msg)
+            raise newException(Exception, error_msg)
 
         b.sdlRoot = sdlHome
 
