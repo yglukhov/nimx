b57f0a76a62f28ae8e0f484d12f01be7773d58f1
Volodymyr Melnychuk <valdema_r@hotmail.com>
2019-01-02 13:34:01 +0200

    support of lattest SDL's gradle build system (#352)

--- test/android/template_ant/jni/Application.mk
+++
@@ -1,7 +0,0 @@
-
-# Uncomment this if you're using STL in your project
-# See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
-# APP_STL := stlport_static
-
-APP_ABI := $(TARGET_ARCHITECTURES)
-APP_PLATFORM:= android-$(TARGET_API)
