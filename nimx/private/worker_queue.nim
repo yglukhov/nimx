a8c1261908044c91ff44656b6fe7a08ee625b0eb
Yuriy Glukhov <yglukhov@users.noreply.github.com>
2020-04-10 21:46:52 +0300

    Gc arc fixes (#409)
    * Fixed compilation with --gc:arc
    
    * Fixed compilation with --gc:arc

--- nimx/private/worker_queue.nim
+++ nimx/private/worker_queue.nim
@@ -5,19 +5,23 @@ type TaskListNode = object
     data: pointer
     next: ptr TaskListNode
 
-type WorkerQueue* = ref object
-    queueCond: Cond
-    queueLock: Lock
-    taskList: ptr TaskListNode
-    lastTask: ptr TaskListNode
-    threads: seq[Thread[pointer]]
+type
+    WorkerQueue* = ref WorkerQueueObj
+    WorkerQueueObj = object
+        queueCond: Cond
+        queueLock: Lock
+        taskList: ptr TaskListNode
+        lastTask: ptr TaskListNode
+        threads: seq[Thread[pointer]]
 
-proc finalize(w: WorkerQueue) =
-    w.queueCond.deinitCond()
-    w.queueLock.deinitLock()
+# proc finalize(w: WorkerQueue) =
+#     # This will not work, so it's not used.
+#     # This is a leak! It's used in nimx image because it's global anyway.
+#     w.queueCond.deinitCond()
+#     w.queueLock.deinitLock()
 
 proc threadWorker(qu: pointer) {.thread.} =
-    let q = cast[WorkerQueue](qu)
+    let q = cast[ptr WorkerQueueObj](qu)
     while true:
         var t: ptr TaskListNode
         q.queueLock.acquire()
@@ -37,7 +41,7 @@ proc threadWorker(qu: pointer) {.thread.} =
         deallocShared(t)
 
 proc newWorkerQueue*(maxThreads : int = 0): WorkerQueue =
-    result.new(finalize)
+    result.new()
     result.queueCond.initCond()
     result.queueLock.initLock()
     let mt = if maxThreads == 0: 2 else: maxThreads
@@ -60,6 +64,7 @@ proc addTask*(q: WorkerQueue, p: proc(data: pointer) {.cdecl, gcsafe.}, data: po
     q.queueLock.release()
 
 when isMainModule:
+    import os
     proc worker(data: pointer) {.cdecl.} =
         echo "worker doing: ", cast[int](data)
         sleep(500)
