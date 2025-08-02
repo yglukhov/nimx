bbd342a789ea092b778e611b08080d4f64fcabef
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2025-08-02 22:11:51 +0200

    wasm progress

--- nimx/private/windows/web_window.nim
+++ nimx/private/windows/web_window.nim
@@ -1,11 +1,6 @@
+when not defined(wasm):
+  {.error: "This module can be used only for wasm target".}
 
-when defined(js) or defined(emscripten) or defined(wasm):
-  discard
-else:
-  {.error: "This module can be used only for web target".}
-
-# import dom except Window
-# import jsbind
 import wasmrt
 
 import nimx/[ abstract_window, system_logger, view, context, matrixes, app,
@@ -14,7 +9,6 @@ import nimx/private/js_vk_map
 
 type WebWindow* = ref object of Window
   renderingContext: GraphicsContext
-  # canvas: Element
   canvasId: string
 
 proc fullScreenAvailableAux(): int {.importwasmraw: """
@@ -51,7 +45,6 @@ proc exitFullscreen() {.importwasmraw: """
 
 method `fullscreen=`*(w: WebWindow, v: bool) =
   let isFullscreen = w.fullscreen
-  # let c = w.canvas
 
   if not isFullscreen and v:
     requestFullscreen(w.canvasId)
@@ -60,23 +53,8 @@ method `fullscreen=`*(w: WebWindow, v: bool) =
 
 export abstract_window
 
-# template buttonStateFromKeyEvent(evt: dom.Event): ButtonState =
-#   if evt.`type` == "keyup": bsUp
-#   elif evt.`type` == "keydown": bsDown
-#   else: bsUnknown
-
 # proc setupWebGL() =
 #   {.emit: """
-#     window.requestAnimFrame = (function() {
-#       return window.requestAnimationFrame ||
-#         window.webkitRequestAnimationFrame ||
-#         window.mozRequestAnimationFrame ||
-#         window.oRequestAnimationFrame ||
-#         window.msRequestAnimationFrame ||
-#         function(callback, element) {
-#         window.setTimeout(callback, 1000/60);
-#     };
-#   })();
 
 #   window.__nimx_focused_canvas = null;
 
@@ -84,93 +62,12 @@ export abstract_window
 #     window.__nimx_focused_canvas = event.target;
 #   }, false);
 
-#   window.__nimx_keys_down = {};
 #   """.}
-
-#   proc onkey(evt: dom.Event) =
-#     when declared(KeyboardEvent):
-#       let evt = cast[KeyboardEvent](evt)
-#     var wnd : WebWindow
-#     var repeat = false
-#     let bs = buttonStateFromKeyEvent(evt)
-#     if bs == bsDown:
-#       {.emit: """
-#       `repeat` = `evt`.keyCode in window.__nimx_keys_down;
-#       window.__nimx_keys_down[`evt`.keyCode] = true;
-#       """.}
-#     elif bs == bsUp:
-#       {.emit: """
-#       delete window.__nimx_keys_down[`evt`.keyCode];
-#       """.}
-
-#     {.emit: """
-#     if (window.__nimx_focused_canvas !== null && window.__nimx_focused_canvas.__nimx_window !== undefined) {
-#       `wnd` = window.__nimx_focused_canvas.__nimx_window;
-#     }
-#     """.}
-#     if not wnd.isNil:
-#       # TODO: Complete this!
-#       var e = newKeyboardEvent(virtualKeyFromNative(evt.keyCode), bs, repeat)
-
-#       #result.rune = keyEv.keysym.unicode.Rune
-#       e.window = wnd
-#       discard mainApplication().handleEvent(e)
-
-#   document.addEventListener("keydown", onkey, false)
-#   document.addEventListener("keyup", onkey, false)
 
 
 # setupWebGL()
 
-# proc buttonCodeFromJSEvent(e: dom.Event): VirtualKey =
-#   when declared(MouseEvent):
-#     let e = cast[MouseEvent](e)
-#   case e.button:
-#     of 1: VirtualKey.MouseButtonPrimary
-#     of 2: VirtualKey.MouseButtonSecondary
-#     of 3: VirtualKey.MouseButtonMiddle
-#     else: VirtualKey.Unknown
-
-# proc eventLocationFromJSEvent(e: dom.Event, c: Element): Point =
-#   var offx, offy: Coord
-#   {.emit: """
-#   var r = `c`.getBoundingClientRect();
-#   `offx` = r.left;
-#   `offy` = r.top;
-#   """.}
-#   when declared(MouseEvent):
-#     let e = cast[MouseEvent](e)
-#   result.x = e.clientX.Coord - offx
-#   result.y = e.clientY.Coord - offy
-
 # proc setupEventHandlersForCanvas(w: WebWindow, c: Element) =
-#   let onmousedown = proc (e: dom.Event) =
-#     var evt = newMouseDownEvent(eventLocationFromJSEvent(e, c), buttonCodeFromJSEvent(e))
-#     evt.window = w
-#     discard mainApplication().handleEvent(evt)
-
-#   let onmouseup = proc (e: dom.Event) =
-#     var evt = newMouseUpEvent(eventLocationFromJSEvent(e, c), buttonCodeFromJSEvent(e))
-#     evt.window = w
-#     discard mainApplication().handleEvent(evt)
-
-#   let onmousemove = proc (e: dom.Event) =
-#     var evt = newMouseMoveEvent(eventLocationFromJSEvent(e, c))
-#     evt.window = w
-#     discard mainApplication().handleEvent(evt)
-
-#   let onscroll = proc (e: dom.Event): bool =
-#     var evt = newEvent(etScroll, eventLocationFromJSEvent(e, c))
-#     var x, y: Coord
-#     {.emit: """
-#     `x` = `e`.deltaX;
-#     `y` = `e`.deltaY;
-#     """.}
-#     evt.offset.x = x
-#     evt.offset.y = y
-#     evt.window = w
-#     result = not mainApplication().handleEvent(evt)
-
 #   let onresize = proc (e: dom.Event): bool =
 #     var sizeChanged = false
 #     var newWidth, newHeight : Coord
@@ -202,75 +99,70 @@ export abstract_window
 #   let onblur = proc()=
 #     w.onFocusChange(false)
 
-#   # TODO: Remove this hack, when handlers definition in dom.nim fixed.
-#   {.emit: """
-#   document.addEventListener('mousedown', `onmousedown`)
-#   document.addEventListener('mouseup', `onmouseup`)
-#   document.addEventListener('mousemove', `onmousemove`)
-#   document.addEventListener('wheel', `onscroll`)
 
 #   window.onresize = `onresize`;
 #   window.onfocus = `onfocus`;
 #   window.onblur = `onblur`;
 #   """.}
 
-proc setupEventHandlers(
+proc setupMouseEventHandlers(
   c: cstring,
-  m: proc(x, y: cdouble, buttonState, buttonCode: int32) {.nimcall.}
+  m: proc(x, y: cdouble, buttonState, buttonCode: int32, deltaX, deltaY: cdouble): int32 {.nimcall.}
      ) {.importwasmraw: """
   var d = document, c = d.getElementById(_nimsj($0)),
   om = (s, e) => {
     var r = c.getBoundingClientRect();
-    var p = 1;//window.devicePixelRatio || 1;
-    _nime._dvddii($1, (e.clientX - r.left)*p, (e.clientY - r.top)*p, s, e.button)
+    if (_nime._diddiidd($1, e.clientX - r.left, e.clientY - r.top, s, e.button, e.deltaX, e.deltaY))
+      e.preventDefault()
   };
   d.addEventListener('mousemove', e => om(0, e));
   d.addEventListener('mousedown', e => om(1, e));
   d.addEventListener('mouseup', e => om(2, e));
+  c.addEventListener('wheel', e => om(3, e))
+  """.}
+
+proc setupKeyEventHandlers(
+  m: proc(keyCode, repeat, buttonState: int32): int32 {.nimcall.}
+     ) {.importwasmraw: """
+  var d = document,
+  om = (s, e) => {
+    if (_nime._diiii($0, e.keyCode, e.repeat, s)) e.preventDefault()
+  };
+  d.addEventListener('keydown', e => om(1, e));
+  d.addEventListener('keyup', e => om(0, e))
   """.}
 
 proc requestAnimFrame(p: proc() {.nimcall}) {.importwasmraw: """
-requestAnimationFrame(function(){try{_nime._dv($0)}catch(e) {console.log("Error caught", e);_nime.nimerr(); throw e;}})
+requestAnimationFrame(() => {try{_nime._dv($0)}catch(e) {console.log("Error caught", e);_nime.nimerr(); throw e;}})
 """.}
 
 proc animFrame() =
-  try:
-    mainApplication().runAnimations()
-    mainApplication().drawWindows()
-    requestAnimFrame(animFrame)
-  except Exception as e:
-    echo "Exception caught: ", e.msg
-    echo e.getStackTrace()
+  mainApplication().runAnimations()
+  mainApplication().drawWindows()
+  requestAnimFrame(animFrame)
 
-proc getDocumentElementFloatProp(i, p: cstring): cdouble {.importwasmraw: "return document.getElementById(_nimsj($0))[_nimsj($1)]".}
+proc getDocumentElementFloatProp(i, p: cstring): cdouble {.importwasmexpr: "document.getElementById(_nimsj($0))[_nimsj($1)]".}
 proc createCanvas(i: cstring, x, y: cfloat) {.importwasmraw: """
-var d = document, w = window, I = _nimsj($0), e = d.getElementById(I), pixelRatio = w.devicePixelRatio || 1, x = $1, y = $2;
+var d = document, w = window, I = _nimsj($0), e = d.getElementById(I);
 if (!e) {
   e = d.createElement("canvas");
   e.id = I;
   d.body.appendChild(e)
 }
-e.style.width = x + 'px';
-e.style.height = y + 'px';
-// var r = e.getBoundingClientRect();
-e.width = x; // * pixelRatio;
-e.height = y; // * pixelRatio;
-e.scaled = true;
+e.style.width = $1 + 'px';
+e.style.height = $2 + 'px';
 
 if (!w.GLCtx) {
   var o = {stencil: true, alpha: false, premultipliedAlpha: false, antialias: false}, c = null;
   try {
     c = e.getContext('webgl', o)
-  } catch(err) {}
+  } catch(_) {}
   if (!c)
     try {
       c  = e.getContext('experimental-webgl', o)
-    } catch(err) {}
+    } catch(_) {}
 
   if (c) {
-    var devicePixelRatio = 1;//window.devicePixelRatio || 1;
-    c.viewportWidth = x * devicePixelRatio;
-    c.viewportHeight = y * devicePixelRatio;
     c.getExtension('OES_standard_derivatives');
     c.pixelStorei(c.UNPACK_PREMULTIPLY_ALPHA_WEBGL, false);
     w.GLCtx = c
@@ -279,37 +171,48 @@ if (!w.GLCtx) {
 }
 """.}
 
-# proc adjustCanvasPixelRatio(i: cstring)
+proc onMouse(x, y: cdouble, buttonState, buttonCode: int32, dx, dy: cdouble): int32 =
+  let a = mainApplication()
+  let w = a.keyWindow
+  if not w.isNil:
+    let pos = newPoint(x, y)
+    var evt: Event
+    if buttonState == 3:
+      evt = newEvent(etScroll, pos)
+      evt.offset.x = dx.Coord
+      evt.offset.y = dy.Coord
+    else:
+      let buttonState = case buttonState
+                        of 1: bsDown
+                        of 2: bsUp
+                        else: bsUnknown
+      let buttonCode = case buttonCode
+                      of 1: VirtualKey.MouseButtonPrimary
+                      of 2: VirtualKey.MouseButtonSecondary
+                      of 3: VirtualKey.MouseButtonMiddle
+                      else: VirtualKey.Unknown
+      evt = if buttonState == bsUnknown:
+          newMouseMoveEvent(pos)
+        else:
+          newMouseButtonEvent(pos, buttonCode, buttonState)
+    evt.window = w
+    result = a.handleEvent(evt).int32
 
-proc onMouse(x, y: cdouble, buttonState, buttonCode: int32) =
+proc onKey(keyCode, repeat, buttonState: int32): int32 =
   let a = mainApplication()
   let w = a.keyWindow
   if not w.isNil:
     let buttonState = case buttonState
                       of 1: bsDown
-                      of 2: bsUp
-                      else: bsUnknown
-    let buttonCode = case buttonCode
-                     of 1: VirtualKey.MouseButtonPrimary
-                     of 2: VirtualKey.MouseButtonSecondary
-                     of 3: VirtualKey.MouseButtonMiddle
-                     else: VirtualKey.Unknown
-    var pos = newPoint(x, y)
-    var evt = if buttonState == bsUnknown:
-        newMouseMoveEvent(pos)
-      else:
-        newMouseButtonEvent(pos, buttonCode, buttonState)
-    evt.window = w
-    try:
-      discard mainApplication().handleEvent(evt)
-    except Exception as e:
-      echo "Exception caught: ", e.msg
-      echo e.getStackTrace()
+                      else: bsUp
+    var e = newKeyboardEvent(virtualKeyFromNative(keyCode), buttonState, bool(repeat))
+    e.window = w
+    result = a.handleEvent(e).int32
 
 proc initWithCanvasId*(w: WebWindow, canvasId: string) =
   w.canvasId = canvasId
-  let width = getDocumentElementFloatProp(canvasId, "width")
-  let height = getDocumentElementFloatProp(canvasId, "height")
+  let width = getDocumentElementFloatProp(canvasId, "clientWidth")
+  let height = getDocumentElementFloatProp(canvasId, "clientHeight")
   procCall w.Window.init()
   w.onResize(newSize(width, height))
 
@@ -321,8 +224,11 @@ proc initWithCanvasId*(w: WebWindow, canvasId: string) =
   mainApplication().addWindow(w)
   requestAnimFrame(animFrame)
 
-  defineDyncall("vddii")
-  setupEventHandlers(canvasId, onMouse)
+  defineDyncall("iddiidd")
+  setupMouseEventHandlers(canvasId, onMouse)
+
+  defineDyncall("iiii")
+  setupKeyEventHandlers(onKey)
 
 proc nextCanvasId(): string =
   var counter {.global.} = 0
@@ -340,7 +246,6 @@ proc initByFillingBrowserWindow*(w: WebWindow) =
 #   result.initWithCanvasId(canvasId)
 
 proc newWebWindow*(r: Rect): Window =
-  echo "new web window"
   WebWindow.new()
 
 proc newWebWindowByFillingBrowserWindow*(): Window =
@@ -352,11 +257,35 @@ proc newWebWindowByFillingBrowserWindow*(): Window =
 newWindow = newWebWindow
 newFullscreenWindow = newWebWindowByFillingBrowserWindow
 
+proc listenPixelRatioChange(cb: proc() {.nimcall.}) {.importwasmraw: """
+  matchMedia(
+    `(resolution: ${window.devicePixelRatio}dppx)`
+  ).addEventListener("change", () => _nime._dv($0), { once: true })
+""".}
+
+proc getPixelRatio(): float {.importwasmexpr: "window.devicePixelRatio || 1".}
+
+proc updateCanvasPixelRatio(r: float, canvasId: cstring) {.importwasmraw: """
+  var c = document.getElementById(_nimsj($1));
+  c.width = c.clientWidth * $0;
+  c.height = c.clientHeight * $0
+""".}
+
+proc onPixelRatioChange() =
+  let a = mainApplication()
+  let w = a.keyWindow
+  if not w.isNil:
+    w.onResize(w.frame.size)
+
+  defineDyncall("v")
+  listenPixelRatioChange(onPixelRatioChange)
+
 method init*(w: WebWindow) =
   let id = nextCanvasId()
   createCanvas(id, 800, 600)
   # init(cast[pointer](w), w.canvasId, $r.width, $r.height)
   w.initWithCanvasId(id)
+  onPixelRatioChange()
 
 method drawWindow*(w: WebWindow) =
   let c = w.renderingContext
@@ -369,40 +298,62 @@ method onResize*(w: WebWindow, newSize: Size) =
   w.renderingContext.gl.viewport(0, 0, GLSizei(newSize.width), GLsizei(newSize.height))
   procCall w.Window.onResize(newSize)
 
-# proc sendInputEvent(wnd: WebWindow, evt: dom.Event) =
-#   var s: cstring
-#   {.emit: """
-#   `s` = window.__nimx_textinput.value;
-#   window.__nimx_textinput.value = "";
-#   """.}
-#   var e = newEvent(etTextInput)
-#   e.window = wnd
-#   e.text = $s
-#   discard mainApplication().handleEvent(e)
+  let p = getPixelRatio()
+  w.viewportPixelRatio = p
+  updateCanvasPixelRatio(p, WebWindow(w).canvasId)
+  let vp = w.frame.size * p
+  sharedGL().viewport(0, 0, GLSizei(vp.width), GLsizei(vp.height))
 
-# method startTextInput*(wnd: WebWindow, r: Rect) =
-#   let oninput = proc(evt: dom.Event) =
-#     wnd.sendInputEvent(evt)
+proc startTextInputAux(cb: proc(a: JSRef) {.cdecl.}) {.importwasmraw: """
+  if (window.__nimx_textinput === undefined) {
+    var i = window.__nimx_textinput = document.createElement('input');
+    i.type = 'text';
+    i.style.position = 'absolute';
+    i.style.top = '-99999px';
+    document.body.appendChild(i)
+  }
+  window.__nimx_textinput.oninput = () => {
+    _nime._dvi($0, _nimok(window.__nimx_textinput.value));
+    window.__nimx_textinput.value = ""
+  };
+  setTimeout(() => window.__nimx_textinput.focus(), 1)
+""".}
 
-#   {.emit: """
-#   if (window.__nimx_textinput === undefined) {
-#     var i = window.__nimx_textinput = document.createElement('input');
-#     i.type = 'text';
-#     i.style.position = 'absolute';
-#     i.style.top = '-99999px';
-#     document.body.appendChild(i);
-#   }
-#   window.__nimx_textinput.oninput = `oninput`;
-#   setTimeout(function(){ window.__nimx_textinput.focus(); }, 1);
-#   """.}
+proc length(j: JSObj): int {.importwasmp.}
+proc strWriteOut(j: JSObj, p: pointer, len: int): int {.importwasmf: "_nimws".}
 
-# method stopTextInput*(w: WebWindow) =
-#   {.emit: """
-#   if (window.__nimx_textinput !== undefined) {
-#     window.__nimx_textinput.oninput = null;
-#     window.__nimx_textinput.blur();
-#   }
-#   """.}
+proc jsStringToStr(v: JSObj): string =
+  if not v.isNil:
+    let sz = length(v) * 3
+    result.setLen(sz)
+    if sz != 0:
+      let actualSz = strWriteOut(v, addr result[0], sz)
+      result.setLen(actualSz)
+
+proc onInput(a: JSRef) {.cdecl.} =
+  var e = newEvent(etTextInput)
+  e.text = block:
+    # Force JSRef destruction early
+    jsStringToStr(JSObj(o: a))
+
+  let a = mainApplication()
+  e.window = a.keyWindow
+  if not e.window.isNil:
+    discard a.handleEvent(e)
+
+method startTextInput*(w: WebWindow, r: Rect) =
+  defineDyncall("vi")
+  startTextInputAux(onInput)
+
+proc stopTextInputAux() {.importwasmraw: """
+  if (window.__nimx_textinput !== undefined) {
+    window.__nimx_textinput.oninput = null;
+    window.__nimx_textinput.blur()
+  }
+""".}
+
+method stopTextInput*(w: WebWindow) =
+  stopTextInputAux()
 
 # window.onload = () => _nime._dv(p)
 # TODO: the main code should be called upon window.onload, but it doesn't
